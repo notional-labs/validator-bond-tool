91b1b8f8743df56f983c91499d9fabfeab628b59
sontrinh16 <trinhleson2000@gmail.com>
2023-09-13 16:01:21 +0700

    init

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,178 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { Input, Text, ChakraProvider, Center, Link, Flex, Grid, Button, Heading, Box, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
+import { MsgValidatorBond } from '@hoangdv2429/liquid_staking/dist/codegen/staking/v1beta1/tx';
+import { SigningStargateClient } from "@cosmjs/stargate";
+import { AminoConverter } from '@hoangdv2429/liquid_staking/dist/codegen/staking/v1beta1/tx.amino';
+
+const chanId = 'cosmoshub-4'
+const defaultGas = '40000'
+const rpc = 'https://rpc-cosmoshub-ia.cosmosia.notional.ventures/'
+const typeUrl = '/liquidstaking.staking.v1beta1.MsgValidatorBond'
+
+const getOfflineSigner = async (chainId) => {
+  try {
+    if (!window.getOfflineSigner || !window.keplr) {
+      alert("Keplr Wallet not detected, please install extension");
+      throw new Error("Keplr not found");
+    } else {
+      try {
+        await window.keplr.enable(chainId);
+      } catch (e) {
+        const experimentalChain = chainObj[chainId];
+        if (!experimentalChain) throw e;
+        await window.keplr.experimentalSuggestChain(experimentalChain);
+      }
+      const offlineSigner = window.keplr.getOfflineSigner(chainId);
+      const account = await window.keplr.getKey(chainId);
+      return {
+        account,
+        offlineSigner,
+      };
+    }
+  } catch (e) {
+    throw e;
+  }
+};
+
+const getStargateClient = async (signer, rpc) => {
+  const client = await SigningStargateClient.connectWithSigner(rpc, signer);
+  client.aminoTypes.register[typeUrl] = {
+    aminoType: AminoConverter[typeUrl].aminoType,
+    toAmino: AminoConverter[typeUrl].toAmino,
+    fromAmino: AminoConverter[typeUrl].fromAmino,
+  }
+
+  client.registry.register(typeUrl, MsgValidatorBond)
+
+  console.log(client.registry.types.get(typeUrl))
+  return client;
+};
+
 
 export default function Home() {
+  const toast = useToast()
+  const [info, setInfor] = useState({
+    delegatorAddress: '',
+    validatorAddress: ''
+  })
+
+  const handleChange = (key, value) => {
+    let newInfor = info
+    newInfor[key] = value
+    setInfor({
+      ...newInfor
+    })
+  }
+
+  const validatorBond = async () => {
+    try {
+      const msgBody = MsgValidatorBond.fromPartial({
+        ...info,
+      })
+
+      const msg = {
+        typeUrl: typeUrl,
+        value: msgBody,
+      };
+      const { account, offlineSigner } = await getOfflineSigner(
+        chanId
+      );
+      const fee = {
+        amount: [],
+        gas: defaultGas,
+      };
+
+      const stargate = await getStargateClient(offlineSigner, rpc);
+      console.log(stargate.registry)
+      if (stargate != null) {
+        await stargate.sign(account.bech32Address, [msg], fee, "");
+      }
+      toast({
+        position: 'top-right',
+        status: 'success',
+        render: () => (
+          <Box color='white' p={4} bg='#58ed80'>
+            Successful
+          </Box>
+        ),
+      })
+    } catch (e) {
+      toast({
+        position: 'top-right',
+        render: () => (
+          <Box color='white' p={4} bg='#f5230a'>
+            {e.message}
+          </Box>
+        ),
+      })
+    }
+  }
+
+  const checkDisable = () => {
+    if (info.delegatorAddress === '' || info.validatorAddress === '') {
+      return true
+    }
+    return false
+  }
+
   return (
-    <div className={styles.container}>
+    <ChakraProvider>
       <Head>
-        <title>Create Next App</title>
+        <title>Validator bond</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Flex direction={'column'} justify={'space-between'} height={'100vh'}>
+        <Box padding={'2em 0'}>
+          <Center>
+            <Heading>
+              Validator Bond Tool
+            </Heading>
+          </Center>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          <Center>
+            <Grid w={'50%'} gap={'50px'} padding={'2rem'}>
+              <Center gap={1}>
+                <Text w={'20%'}>
+                  Delegator address
+                </Text>
+                <Input placeholder='address here' onBlur={(e) => handleChange('delegatorAddress', e.target.value)} />
+              </Center>
+              <Center>
+                <Text w={'20%'}>
+                  Validator address
+                </Text>
+                <Input placeholder='address here' onBlur={(e) => handleChange('validatorAddress', e.target.value)} />
+              </Center>
+              <Button color='white' backgroundColor={'#ff513d'} _hover={{ backgroundColor: '#ad3628' }} onClick={validatorBond} isDisabled={checkDisable()}>
+                Validator Bond
+              </Button>
+            </Grid>
+          </Center>
+        </Box>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <footer style={{
+          backgroundColor: '#2b2b2b',
+          color: 'white'
+        }}>
+          <Center gap={1}>
+            <Text>
+              Powered by
+            </Text>
+            <Link
+              href="https://notional.ventures/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                color: '#85c8ff'
+              }}
+            >
+              Notional
+            </Link>
+          </Center>
+        </footer>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
+        <style jsx>{`
         main {
           padding: 5rem 0;
           flex: 1;
@@ -104,7 +216,7 @@ export default function Home() {
         }
       `}</style>
 
-      <style jsx global>{`
+        <style jsx global>{`
         html,
         body {
           padding: 0;
@@ -126,6 +238,7 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
+      </Flex>
+    </ChakraProvider>
   );
 }
